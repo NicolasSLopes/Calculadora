0c40f68a8a59cf89bb630af6a5cb4493774d99b4
Nicolas <nicolas_s_lopes@estudante.sesisenai.org.br>
2026-04-01 12:55:09 -0300

    add

---
+++ script.js
@@ -0,0 +1,56 @@
+const num1 = document.getElementByld("num1");
+const num2 = document.getElementByld("num2");
+const resultado = document.getElementByld("resultado");
+
+//Função de validação
+function validar(){
+    if (num1value === ""|| num2.value===""){
+        resultado.innerText = "Erro: Preencha todos os campos!";
+        return false;
+    }
+}
+if (isNaN(num1.value)||isNaN(num2.value)){
+    resultado.innerText = "Erro: digite apenas numeros!";
+    return false;
+}
+//Funções matemáticas
+function somar(a,b){
+    return a+b;
+}
+
+function subtrair (a,b){
+    return a-b;
+}
+function muliplicar(a,b){
+    return a*b;
+}
+
+function dividir(a,b){
+    if (b===0){
+        return "Erro: Não é possivel dividir por 0!";
+    }
+    return a/b;
+}
+
+
+document getElementById("multiplicacao").addEventListener("click", () => {
+    if(!validar()) return;
+    const a = parseFloat(num1.value);
+    const b = parseFloat(num2.value);
+    resultado.innerText = "Resultado" + multiplicar(a, b);
+});
+
+document getElementById("divisao").addEventListener("click", () => {
+    if(!validar()) return;
+    const a = parseFloat(num1.value);
+    const b = parseFloat(num2.value);
+    resultado.innerText = "Resultado" + dividir(a, b);
+});
+
+//Limpando
+document getElementById("limpar").addEventListener("click", () => {
+    num1.value = "";
+    num2.value = "";
+    resultado.innerText = "Resultado";
+});
+
